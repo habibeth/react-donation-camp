76b1858d90165ec0b17e7d0a561a46dd09214eaf
Ahsan Habib <habibstk97@gmail.com>
2023-12-16 21:31:16 +0600

    work is almost done 1

--- src/components/DonationDetails/DonationDetails.jsx
+++ src/components/DonationDetails/DonationDetails.jsx
@@ -6,26 +6,38 @@ import DonationDetail from "../DonationDetail/DonationDetail";
 
 const DonationDetails = () => {
     const donationItems = useLoaderData();
+    const [dataLength, setDataLength] = useState(4);
     const [donationItem, setDonationItem] = useState([]);
 
-    useEffect(()=>{
+    useEffect(() => {
         const storedDonationIds = getLocalStorageDonation();
         const savedDonationItem = [];
-        for( const Ids of storedDonationIds){
-            const donation = donationItems.find(item=> item.id === Ids);
+        for (const Ids of storedDonationIds) {
+            const donation = donationItems.find(item => item.id === Ids);
             savedDonationItem.push(donation);
             setDonationItem(savedDonationItem);
         }
-    },[donationItems])
+    }, [donationItems])
 
     console.log(donationItem)
-    
+
     console.log('donation page')
     return (
         <div className="max-w-7xl mx-auto grid grid-cols-2 gap-5 mb-20 mt-20">
-            {
-                donationItem.map(savedItem=> <DonationDetail key={savedItem.id} savedItem={savedItem}></DonationDetail>)
-            }
+            <div className="">
+                {
+                    donationItem.slice(0, dataLength).map(savedItem => <DonationDetail
+                        key={savedItem.id}
+                        savedItem={savedItem}
+                    ></DonationDetail>)
+                }
+            </div>
+
+            <div className={ dataLength === donationItems.length ? 'hidden' : 'flex justify-center'}>
+                <button className="btn mt-10 w-32 bg-gradient-to-r from-sky-500 to-indigo-500 text-white"
+                    onClick={() => setDataLength(donationItems.length)}
+                >See All</button>
+            </div>
         </div>
     );
 };
